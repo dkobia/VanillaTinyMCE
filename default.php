d3f1ab156ebc448e699a2b9e8454f2447cf05901
Steve Lawson <smlsml@gmail.com>
2011-07-19 18:19:16 -0700

    Changes to the basic theme

--- default.php
+++ default.php
@@ -184,12 +184,12 @@ EOF;
 						plugins : "pagebreak,style,layer,table,save,advhr,advimage,advlink,emotions,iespell,inlinepopups,insertdatetime,preview,media,searchreplace,print,contextmenu,paste,directionality,fullscreen,noneditable,visualchars,nonbreaking,xhtmlxtras,template,advlist",
 
 						// Theme options
-						theme_advanced_buttons1 : "save,newdocument,|,bold,italic,underline,strikethrough,|,justifyleft,justifycenter,justifyright,justifyfull,styleselect,formatselect,fontselect,fontsizeselect",
+						theme_advanced_buttons1 : ",bold,italic,underline,strikethrough,|,justifyleft,justifycenter,justifyright,justifyfull,styleselect,formatselect,fontselect,fontsizeselect",
 						theme_advanced_buttons2 : "bullist,numlist,|blockquote,|,link,unlink,image,|,preview,|,forecolor,",
 						theme_advanced_buttons3 : "",
 						theme_advanced_toolbar_location : "top",
 						theme_advanced_toolbar_align : "left",
-						theme_advanced_statusbar_location : "bottom",
+						theme_advanced_statusbar_location : "none",
 						theme_advanced_resizing : true,
 
 						// Example content CSS (should be your site CSS)
